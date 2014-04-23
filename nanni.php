nschneid/nanni

commit f88a3a2b7e13defb45bd65de00b663d3914a01ec

always display disabled but nonempty inputs, even in previous versions. fixes #5.

nanni.php
@@ -1501,7 +1501,7 @@ ChunkLabelAnnotator.prototype.identifyTargets = function() {
 			if (this.ann.target.value) {
 				var v = $.parseJSON(this.ann.target.value)[this.tokenOffset];
 				if (v) {
-					v = v.split('|');
+					v = v.split('|');	// if of the form word|LABEL, strip the first part
 					val = v[v.length-1];
 				}
 			}
@@ -1596,6 +1596,7 @@ ChunkLabelAnnotator.prototype.updateTargets = function(updateInfo) {
 			else {
 				$(a.target).prop("disabled","disabled").prop("required","");
 			}
+			if (theann.constructor.started) a.aparecium();
 		}
 		else {
 			// irrelevant given MWE analysis

commit 6fb5bd71ab552a0067cefd404904b2229183f671

f88a3a2b7e13defb45bd65de00b663d3914a01ec caused all irrelevant (empty) label input boxes to show up as disabled when changing the MWE analysis

nanni.php
@@ -1596,7 +1596,7 @@ ChunkLabelAnnotator.prototype.updateTargets = function(updateInfo) {
 			else {
 				$(a.target).prop("disabled","disabled").prop("required","");
 			}
-			if (theann.constructor.started) a.aparecium();
+			if (theann.constructor.started && a.submittable) a.aparecium();
 		}
 		else {
 			// irrelevant given MWE analysis